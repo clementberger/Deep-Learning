7abdf7c88372f54fd45235ac945904e2847394eb
Clément Berger <clement.berger97@gmail.com>
2021-01-23 21:46:47 +0100

    modif

--- mir/attention.py
+++ mir/attention.py
@@ -86,3 +86,44 @@ class CNN_BiLSTM_Attention(nn.Module):
         x = torch.sum(norm_att * bil, dim = 2)
         x = F.hardtanh(x, 0., 1.)
         return x
+
+class CNN_reduit_BiLSTM_Attention(nn.Module):
+    def __init__(self):
+        super(CNN_reduit_BiLSTM_Attention, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 8, kernel_size = 3)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size = 3)
+        self.att = nn.Conv2d(16*30, 20, kernel_size = 1)
+        self.bilstm = nn.LSTM(16*30, 20, batch_first = True, bidirectional = True)
+        
+    def forward(self, x):
+        """ CNN
+        """
+        x = x.transpose(1,3)
+        x = x.transpose(2,3)
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.transpose(2,3)
+        x = x.transpose(1,3)
+        a, b, c, d = x.size()
+        x = F.dropout(x.view(a, b, c*d))
+        
+        """attention
+        """
+        att = torch.sigmoid(self.att(x.transpose(1,2)[:,:,:,None].contiguous()))
+        att = att[:, :, :, 0]
+        eps = 1e-7
+        att = torch.clamp(att, eps, 1 - eps)
+        norm_att = att / torch.sum(att, dim=2)[:, :, None]
+        """BiLSTM
+        """
+        bil, (_,_) = self.bilstm(x)
+        a, b, c = bil.size()
+        bil = bil.view(a, b, 2, c//2)
+        bil = (bil[:,:,0,:] + bil[:,:,1,:]) / 2
+        bil = bil.transpose(1,2)
+        """combining for output
+        """
+        x = torch.sum(norm_att * bil, dim = 2)
+        x = F.hardtanh(x, 0., 1.)
+        return x
